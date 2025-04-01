d0f900489ecac5ae45d058d8b94887295c0c5cf5
Austin Keiber <adkeiber@gmail.com>
2025-03-31 22:44:59 -0400

    add login endpoint

--- auth-app/app/Http/Controllers/UserController.php
+++ auth-app/app/Http/Controllers/UserController.php
@@ -6,8 +6,20 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function helloWorld()
-    {
+    public function helloWorld(){
         return response()->json("Hello World"); // Return JSON response
+    }
+
+    public function login(Request $request){
+        
+        // Validate input
+        $request -> validate([
+            'username' => 'required|string',
+            'password'=> 'required|string',
+        ]);
+
+
+
+        return response()->json( $request -> password); // Return JSON response
     }
 }
--- auth-app/routes/api.php
+++ auth-app/routes/api.php
@@ -3,3 +3,4 @@
 use App\Http\Controllers\UserController;
 
 Route::get("/hello", [UserController::class,"helloWorld"]);
+Route::post('/login', [UserController::class, 'login']);
